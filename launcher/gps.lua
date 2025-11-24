76aaa22687cf9122f650dc5274bd0a35b4fb9c2f
ob-105 <owenj4712@GMAIL.COM>
2025-11-23 23:33:17 -0500

    Refactor GPS viewer into a module
    Refactor gps.lua to return a table with GPS functionality.

--- launcher/gps.lua
+++ launcher/gps.lua
@@ -1,18 +1,31 @@
 -- gps.lua
-print("GPS Viewer")
-print("Press Ctrl+T to exit")
-
-while true do
-    local x, y, z = gps.locate(2) -- 2 second timeout
-    term.clear()
-    term.setCursorPos(1,1)
-    if x then
-        print("GPS Connected")
-        print("X: " .. math.floor(x))
-        print("Y: " .. math.floor(y))
-        print("Z: " .. math.floor(z))
-    else
-        print("GPS: No Signal")
+return {
+    name = "GPS",
+    description = "Shows your current GPS coordinates.",
+    image = {
+        "############",
+        "#          #",
+        "#   GPS    #",
+        "#          #",
+        "############",
+        "############"
+    },
+    run = function()
+        print("GPS Viewer")
+        print("Press Ctrl+T to exit")
+        while true do
+            local x, y, z = gps.locate(2)
+            term.clear()
+            term.setCursorPos(1,1)
+            if x then
+                print("GPS Connected")
+                print("X: " .. math.floor(x))
+                print("Y: " .. math.floor(y))
+                print("Z: " .. math.floor(z))
+            else
+                print("GPS: No Signal")
+            end
+            sleep(2)
+        end
     end
-    sleep(2)
-end
+}
