6f8b38d33c07e6d3d50772fd6ec89ae5766367b5
ob-105 <owenj4712@GMAIL.COM>
2025-11-23 23:32:54 -0500

    Refactor chat.lua to use a modular approach
    Refactor chat.lua to encapsulate functionality in a table and improve structure.

--- launcher/chat.lua
+++ launcher/chat.lua
@@ -1,33 +1,41 @@
 -- chat.lua
-local modem = peripheral.find("modem")
+return {
+    name = "Chat",
+    description = "Connect to the global chat channel.",
+    image = {
+        "############",
+        "#          #",
+        "#   Chat   #",
+        "#          #",
+        "############",
+        "############"
+    },
+    run = function()
+        local modem = peripheral.find("modem")
+        if not modem then
+            print("No modem found!")
+            return
+        end
+        modem.open(1)
+        print("Connected to Global Chat (channel 1)")
+        print("Type messages and press Enter. Press Ctrl+T to exit.")
 
-if not modem then
-    print("No modem found!")
-    return
-end
-
--- Open global channel (1)
-modem.open(1)
-
-print("Connected to Global Chat (channel 1)")
-print("Type messages and press Enter. Press Ctrl+T to exit.")
-
-local function sendMessage(msg)
-    modem.transmit(1, 1, msg)
-end
-
-while true do
-    local event, p1, p2, p3, p4 = os.pullEvent()
-
-    if event == "modem_message" then
-        local side, channel, replyChannel, message = p1, p2, p3, p4
-        if channel == 1 then
-            print("Global: " .. tostring(message))
+        local function sendMessage(msg)
+            modem.transmit(1, 1, msg)
         end
 
-    elseif event == "key" and p1 == keys.enter then
-        term.write("> ")
-        local msg = read()
-        sendMessage(msg)
+        while true do
+            local event, p1, p2, p3, p4 = os.pullEvent()
+            if event == "modem_message" then
+                local side, channel, replyChannel, message = p1, p2, p3, p4
+                if channel == 1 then
+                    print("Global: " .. tostring(message))
+                end
+            elseif event == "key" and p1 == keys.enter then
+                term.write("> ")
+                local msg = read()
+                sendMessage(msg)
+            end
+        end
     end
-end
+}
