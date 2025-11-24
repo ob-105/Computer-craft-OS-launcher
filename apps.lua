75527232cc476f25ec1af9919b06e31e5a5717ff
ob-105 <owenj4712@GMAIL.COM>
2025-11-23 22:00:56 -0500

    Define apps with details in apps.lua
    Added a table of applications with names, images, and descriptions.

---
+++ apps.lua
@@ -0,0 +1,27 @@
+-- apps.lua
+local apps = {
+    {
+        name = "Calculator",
+        image = {
+            "############",
+            "#          #",
+            "#   Calc   #",
+            "#          #",
+            "############",
+            "############"
+        },
+        description = "A simple calculator for math operations."
+    },
+    {
+        name = "Chat",
+        image = nil,
+        description = "Connect and chat with other players."
+    },
+    {
+        name = "GPS Viewer",
+        image = nil,
+        description = "Shows your current GPS coordinates."
+    }
+}
+
+return apps
